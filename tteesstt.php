2fb1124af48b7aaffc81ebe5eebf14233cb38416
Bert Ehrmann <bert@fai2.com>
2012-07-16 15:46:01 -0400

    Test
    This is a test

---
+++ tteesstt.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<script>document.cookie='resolution='+480+'; path=/';</script>
+
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<p>asdasdasd121212</p>
+<p><img src="Screen Shot 2012-07-12 at 11.11.07 AM.png" width="866" height="564" /></p>
+</body>
+</html>
