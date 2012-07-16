d99a0f829253680e039930d7774efd3a4211f67a
hickks <bert@fai2.com>
2012-07-16 16:53:22 -0300

    We need to edit the comment.

--- tteesstt.php
+++ tteesstt.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<?php echo "this is a test, yo!"; ?>
+<?php echo "this is a test, yo!"; 
+// This is a comment
+?>
 
 
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
